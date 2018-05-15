a7585bb8143f7c8d7c1494c470ce1ef66aa1b76a
Ninad Sakhardande <ninad.sakhardande@gmail.com>
2018-05-15 14:35:24 +0530

    Added contract

--- src/stack.h
+++ src/stack.h
@@ -7,10 +7,41 @@ struct stack {
 	int (* g_print)(FILE *, void *);
 }
 
+/* Creates the stack
+	
+	Parameters:
+		A compare function
+		A Print Function
+	
+	Returns:
+		Returns the created stack
+*/
 struct stack *stack_create(bool (* f)(void *, void *), int (* g)(FILE *, void *)) ;
+
+/* Pushes the element dp onto the stack
+	
+	Parameters:
+		A void pointer dp to be pushed on the stack
+		The stack
+	
+	Returns:
+		A boolean value to indicate whether element has been pushed on the stack or not
+*/
 bool stack_push(void *dp, struct stack *s) ;
+
+/* Removes the top most element from the stack
+
+	Parameter:
+		The stack
+
+	Returns:
+		The value of the removed element
+*/
 void *stack_pop(struct stack *s) ;
-void *stack_top(struct stack *s) ;
+
+// Returns a boolean value to indicate whether the stack is empty or not
 bool is_empty(struct stack *s) ;
+
+// Destroys the stack
 void stack_destroy(struct stack *s) ;
 
