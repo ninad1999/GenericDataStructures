a2bdbc2bc9dcee0b933fb62c8daab239dd5b4ed9
Ninad Sakhardande <ninad.sakhardande@gmail.com>
2018-05-15 11:10:29 +0530

    Added more test cases.

--- src/test_glist.c
+++ src/test_glist.c
@@ -8,13 +8,36 @@ void quick_tests()
 	struct glist *pintl = glist_create(are_equal_int, fprint_int);
 	int elements[] = {1, 2, 3, 4, 5, 6};
 
-	add_back(elements + 0, pintl);
-	add_back(elements + 1, pintl);
-	add_back(elements + 2, pintl);
-	add_back(elements + 3, pintl);
-	add_back(elements + 4, pintl);
-	add_back(elements + 5, pintl);
+	add_front(elements + 0, pintl);
+	add_front(elements + 1, pintl);
+	add_front(elements + 2, pintl);
+	add_front(elements + 3, pintl);
+	add_front(elements + 4, pintl);
+	add_front(elements + 5, pintl);
 
+	print_glist(stdout, pintl);
+
+	int elements2[] = {8, 9 ,10, 11, 12, 13};
+
+	add_back(elements2 + 0, pintl);
+	add_back(elements2 + 1, pintl);
+	add_back(elements2 + 2, pintl);
+	add_back(elements2 + 3, pintl);
+	add_back(elements2 + 4, pintl);
+	add_back(elements2 + 5, pintl);
+
+	int elements3[] = {14, 15, 16, 17, 18, 19};
+
+	insert_at(0, elements3 + 0, pintl);
+	insert_at(1, elements3 + 1, pintl);
+	insert_at(2, elements3 + 2, pintl);
+	insert_at(3, elements3 + 3, pintl);
+	insert_at(4, elements3 + 4, pintl);
+	insert_at(-1, elements3 + 5, pintl);
+
+	add_front(NULL, pintl);
+	add_back(NULL, pintl);
+	insert_at(3, NULL, pintl);
 	print_glist(stdout, pintl);
 
 	int pos = glist_lookup(elements + 3, pintl);
@@ -35,7 +58,19 @@ void quick_tests()
 
 	glist_reverse_r(pintl);
 	print_glist(stdout, pintl);
+	
+	int len = glist_len(pintl);
+	printf("%d\n", len);
 
+	for (int i = len; i >= 0; i--) {
+		remove_back(pintl);
+	}
+
+	print_glist(stdout, pintl);
+	
+	remove_back(pintl);
+	print_glist(stdout, pintl);
+	
 	delete_glist(pintl);
 }
 
