c3b0c1f7c25b5c4f8c66c037358bf00e9077c699
ninad1999 <34257987+ninad1999@users.noreply.github.com>
2018-05-15 17:09:54 +0530

    Update stack.h

--- src/stack.h
+++ src/stack.h
@@ -41,7 +41,7 @@ bool stack_push(void *dp, struct stack *s) ;
 void *stack_pop(struct stack *s) ;
 
 // Returns a boolean value to indicate whether the stack is empty or not
-bool is_empty(struct stack *s) ;
+bool is_empty(const struct stack *s) ;
 
 // Destroys the stack
 void stack_destroy(struct stack *s) ;
