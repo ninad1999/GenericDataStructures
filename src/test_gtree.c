4b96e4ef0dcad4b9153a63b03d9930c27b420660
Ninad Sakhardande <ninad.sakhardande@gmail.com>
2018-05-19 11:08:00 +0530

    Testing file for gtree

---
+++ src/test_gtree.c
@@ -0,0 +1,55 @@
+#include "gtree.h"
+#include "gdsutils.h"
+
+int quick_test(void) {
+	
+	struct gtree *gtp = create_gtree(compare_int, fprint_int);
+	int elements[] = {6, 2, 3, 1, 9, 5, 7};
+
+	gtree_insert(elements, gtp);
+	gtree_insert(elements + 1, gtp);
+	gtree_insert(elements + 2, gtp);
+	gtree_insert(elements + 3, gtp);
+	gtree_insert(elements + 4, gtp);
+	gtree_insert(elements + 5, gtp);
+	gtree_insert(elements + 6, gtp);
+	gtree_insert(NULL, gtp);
+	
+	print_gtree(gtp);
+
+	print_inorder(gtp);
+	print_postorder(gtp);
+	print_preorder(gtp);
+
+	int look = gtree_lookup(elements + 4, gtp);
+	int i = 10;
+	int look2 = gtree_lookup(&i, gtp);
+	printf("%d  %d\n", look, look2);
+
+	gtree_remove(elements + 3, gtp);
+	gtree_remove(NULL, gtp);
+	gtree_remove(elements + 4, gtp);
+	gtree_remove(elements + 2, gtp);
+	gtree_remove(elements + 1, gtp);
+	gtree_remove(elements , gtp);
+	gtree_remove(elements + 5, gtp);
+	gtree_remove(elements + 6, gtp);
+	gtree_remove(NULL, gtp);
+
+	print_gtree(gtp);
+
+	gtree_destroy(gtp);
+	
+	return 0;
+}
+
+int main(void) {
+
+	quick_test();
+
+	return 0;
+
+}
+
+
+
